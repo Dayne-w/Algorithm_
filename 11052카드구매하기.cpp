aee5b2f7dea65815a4c78b44226541bd6a794e0f
Dayne-w <108070094+Dayne-w@users.noreply.github.com>
2023-02-11 16:55:00 +0900

    baekjoon 11052번

---
+++ 11052카드구매하기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int count,num, temp;
+    cin >> count;
+    int arr[count+1], result[count+1];
+    arr[0] = 0;
+    for(int i = 1; i <= count; i++){
+        cin >> arr[i];
+    }
+    result[0] = 0;
+    result[1] = arr[1];
+    result[2] = max(arr[1]+arr[1], arr[2]);
+    for(int j = 3; j <= count; j++){
+        temp = arr[1] + result[j-1];
+        for(int k = 2; k <= j; k++){
+            temp = max(arr[k] + result[j-k], temp);
+        }
+        result[j] = temp;
+    }
+    cout << result[count];
+}
