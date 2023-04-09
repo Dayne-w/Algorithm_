e1f898b317810c6f15f726d15f3ff7c7c95411fa
Dayne-w <108070094+Dayne-w@users.noreply.github.com>
2023-04-09 16:48:07 +0900

    Update 11650좌표정렬하기.cpp

--- 11650좌표정렬하기.cpp
+++ 11650좌표정렬하기.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(void){
+bool cmp(pair<int, int> p1, pair<int, int> p2){
+    if(p1.first == p2.first){
+        return p1.second < p2.second;
+    }else{
+        return p1.first < p2.first;
+    }
+}
+
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    vector<pair<int, int>> v;
-    int n;
+    int n, a, b;
+    vector<pair<int,int>> v;
     cin >> n;
-    int x,y;
-    v.push_back({0.,0});
-    for(int i = 1; i <= n; i++){
-        cin >> x >> y;
-        v.push_back({x, y});
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        v.push_back(pair<int,int>(a,b));
     }
-    sort(v.begin(),v.end());
-    for(int i = 1; i <= n; i++){
+    sort(v.begin(), v.end(), cmp);
+    for(int i = 0; i < n; i++){
         cout << v[i].first << ' ' << v[i].second << '\n';
     }
-    return 0;
 }
